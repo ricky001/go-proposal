5b4e85d85daa7e41ff16df7ac0110d7e8f3d4fff
Somya Chawla <somyachawlacs2008@gmail.com>
2024-05-05 23:13:56 +0530

    Add files via upload

--- server.go
+++ server.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "index.html")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "index.html")
+		} else {
+			http.NotFound(w, r)
+		}
+	})
 
-    log.Println("Server started at :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// Serve the JPEG image
+	http.HandleFunc("/heart.jpg", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "heart.jpg")
+	})
+
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
